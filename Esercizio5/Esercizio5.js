328499628ca78e1809a926929c7b51f91068f0f8
BelalBvc <therealbilalnazih@gmail.com>
2025-02-04 12:05:13 +0100

    esercizio 5

---
+++ Esercizio5/Esercizio5.js
@@ -0,0 +1,5 @@
+const iscrittiOnline = ['Gino', 'Pino', 'Roberto'];
+const iscrittiOffline = ['Franca', 'Salvo', 'Maria'];
+
+let nuovoArray = [...iscrittiOnline,...iscrittiOffline]
+console.log(nuovoArray)
